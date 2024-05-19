b90bff4c040168d93e8c82e9d83ab0921a375f86
tgoldfeder <tgoldfeder@augury.com>
2024-05-19 12:54:16 +0300

    setting under construction title in overview

--- st_dashboard/overview.py
+++ st_dashboard/overview.py
@@ -3,10 +3,5 @@ import pandas as pd
 
 
 def overview():
-    st.write("Stocks Overview")
-    # Placeholder DataFrame
-    df = pd.DataFrame({
-        'Column 1': [1, 2, 3, 4],
-        'Column 2': ['A', 'B', 'C', 'D']
-    })
-    st.dataframe(df)
+    st.write("Under Construction")
+
